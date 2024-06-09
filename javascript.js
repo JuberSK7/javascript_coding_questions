3b048732d77cbd80513fbb50eddaf7cccbea6417
ShaikhZuber07 <90678325+JuberSK7@users.noreply.github.com>
2024-06-09 16:10:06 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -19,3 +19,42 @@ const removeDuplicte = [...new Set(arr) ];
 console.log(removeDuplicte); //[ 1, 2, 3, 4, 5, 6, 8, 9];
 
 
+2) // Find the largest or maximum number in the array
+
+const arr = [1,2,3,4,2,5,1,6,8,9,6,8];
+
+// using for loop
+var largeNum = arr[0];
+for(let i=0; i < arr.length; i++){
+   if(arr[i] > largeNum){
+    largeNum = arr[i];
+  }
+}
+
+console.log(largeNum)// 9
+
+///using Math.max() function
+const findMax = Math.max(...arr);
+console.log(findMax)// 9
+
+
+2) // ***Find the smallest or manimum number in the array
+
+const arr = [1,2,3,4,2,5,1,6,8,9,6,8];
+
+// using for loop
+var minNum = arr[0];
+for(let i=0; i < arr.length; i++){
+   if(arr[i] < minNum){
+    minNum = arr[i];
+  }
+}
+
+console.log(minNum)// 1
+
+///using Math.max() function
+const findMax = Math.min(...arr);
+console.log(findMax)// 0
+
+
+
