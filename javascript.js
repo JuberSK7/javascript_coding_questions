f25f9bb2351acff8e0d5c8aff969c90464573fb0
ShaikhZuber07 <90678325+JuberSK7@users.noreply.github.com>
2024-06-29 13:36:54 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -119,3 +119,15 @@ example : -
   <li>User2</li>
   <li>User3</li>
   <ul>
+
+9) ///////////// ***** Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3,
+                      "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5.
+for(let i =0; i < 100; i++){
+  if(i % 3 && i % 5){
+    console.log('fizzbuzz')
+  }else if(i % 3){
+console.log('fizz')
+  }else if(i % 5){
+    console.log('buzz')
+}
+}
